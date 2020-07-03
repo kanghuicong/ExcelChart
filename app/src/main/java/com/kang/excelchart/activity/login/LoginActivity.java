9de46992a2c94512d984b30123536125c28d7916
kanghuicong <515849594@qq.com>
2020-07-04 00:36:42 +0800

    7.4 登录调通

--- app/src/main/java/com/kang/excelchart/activity/login/LoginActivity.java
+++ app/src/main/java/com/kang/excelchart/activity/login/LoginActivity.java
@@ -5,15 +5,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.kang.excelchart.MainActivity;
 import com.kang.excelchart.R;
 import com.kang.excelchart.base.BaseActivity;
 import com.kang.excelchart.bean._User;
+import com.vondear.rxtool.RxActivityTool;
 import com.vondear.rxtool.RxLogTool;
 import com.vondear.rxtool.view.RxToast;
 
 import org.json.JSONArray;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cn.bmob.v3.BmobQuery;
@@ -43,20 +47,44 @@ public class LoginActivity extends BaseActivity {
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String account = etAccount.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if (account.equals("")) {
+                    RxToast.error(getString(R.string.error_account));
+                    return;
+                }
+
+                if (password.equals("")) {
+                    RxToast.error(getString(R.string.error_password));
+                    return;
+                }
+
+                BmobQuery<_User> eq1 = new BmobQuery<>();
+                eq1.addWhereEqualTo("username", account);
+                BmobQuery<_User> eq2 = new BmobQuery<>();
+                eq2.addWhereEqualTo("password", password);
+
+                List<BmobQuery<_User>> queries = new ArrayList<>();
+                queries.add(eq1);
+                queries.add(eq2);
+
                 BmobQuery<_User> query = new BmobQuery<>();
-                query.addWhereEqualTo("username", etAccount.getText().toString())
-                        .setLimit(1)
-                        .findObjects(new FindListener<_User>() {
+                query.and(queries);
+
+                query.findObjects(new FindListener<_User>() {
                     @Override
                     public void done(List<_User> object, BmobException e) {
-                        if (e == null) {
-                            _User usrBean = object.get(0);
-                            RxLogTool.i(usrBean.toString());
-                            if (usrBean.getPassword().equals(etPassword.getText().toString())) {
-                                RxToast.success("success");
+                        if(e==null){
+                            if (object.size() != 0) {
+                                RxActivityTool.skipActivity(activity, MainActivity.class);
+                            }else{
+                                RxToast.error(getString(R.string.error_account_password));
                             }
-                        } else {
-                            RxLogTool.i(e.toString());
+                            RxLogTool.d("查询结果:"+object.size()+"");
+                        }else{
+                            RxLogTool.e(e.toString());
+                            RxToast.error(getString(R.string.error_account_password));
                         }
                     }
                 });
--- app/src/main/java/com/kang/excelchart/bean/_User.java
+++ app/src/main/java/com/kang/excelchart/bean/_User.java
@@ -2,53 +2,15 @@ package com.kang.excelchart.bean;
 
 import java.util.Date;
 
-import cn.bmob.v3.BmobObject;
+import cn.bmob.v3.BmobUser;
 
-public class _User extends BmobObject {
-    private String username;
-    private String password;
-    private boolean mobilePhoneNumberVerified;
-    private String mobilePhoneNumber;
+public class _User extends BmobUser {
     private String appleid;
-    private Date dueTime;
+
     private boolean isVip;
-    private Date rechargeTime;
-    private boolean emailVerified;
-    private String email;
+
     private String authData;
 
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isMobilePhoneNumberVerified() {
-        return mobilePhoneNumberVerified;
-    }
-
-    public void setMobilePhoneNumberVerified(boolean mobilePhoneNumberVerified) {
-        this.mobilePhoneNumberVerified = mobilePhoneNumberVerified;
-    }
-
-    public String getMobilePhoneNumber() {
-        return mobilePhoneNumber;
-    }
-
-    public void setMobilePhoneNumber(String mobilePhoneNumber) {
-        this.mobilePhoneNumber = mobilePhoneNumber;
-    }
 
     public String getAppleid() {
         return appleid;
@@ -58,13 +20,6 @@ public class _User extends BmobObject {
         this.appleid = appleid;
     }
 
-    public Date getDueTime() {
-        return dueTime;
-    }
-
-    public void setDueTime(Date dueTime) {
-        this.dueTime = dueTime;
-    }
 
     public boolean isVip() {
         return isVip;
@@ -74,29 +29,7 @@ public class _User extends BmobObject {
         isVip = vip;
     }
 
-    public Date getRechargeTime() {
-        return rechargeTime;
-    }
 
-    public void setRechargeTime(Date rechargeTime) {
-        this.rechargeTime = rechargeTime;
-    }
-
-    public boolean isEmailVerified() {
-        return emailVerified;
-    }
-
-    public void setEmailVerified(boolean emailVerified) {
-        this.emailVerified = emailVerified;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     public String getAuthData() {
         return authData;
@@ -106,20 +39,6 @@ public class _User extends BmobObject {
         this.authData = authData;
     }
 
-    @Override
-    public String toString() {
-        return "_User{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", mobilePhoneNumberVerified=" + mobilePhoneNumberVerified +
-                ", mobilePhoneNumber='" + mobilePhoneNumber + '\'' +
-                ", appleid='" + appleid + '\'' +
-                ", dueTime=" + dueTime +
-                ", isVip=" + isVip +
-                ", rechargeTime=" + rechargeTime +
-                ", emailVerified=" + emailVerified +
-                ", email='" + email + '\'' +
-                ", authData='" + authData + '\'' +
-                '}';
-    }
+
+
 }
